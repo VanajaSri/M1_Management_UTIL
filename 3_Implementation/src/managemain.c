c21dbfa38497363afec6ca438a6aad8ca6726840
VanajaSri <98846705+VanajaSri@users.noreply.github.com>
2022-02-17 22:16:49 +0530

    Add files via upload

---
+++ 3_Implementation/src/managemain.c
@@ -0,0 +1,58 @@
+#include"inc.h"
+#include"vf.c"
+
+int main()
+{
+  struct library l[100];
+ int count = 0;
+    int j = 0;
+    int  i = 0;
+    char *str;
+
+   /* Initial memory allocation */
+   str = (char *) malloc(15);
+   free(str);
+
+  while (j != 6)
+  {
+    int a = menu();
+
+    printf("\n\nEnter one of the above : ");
+    scanf("%d", &j);
+
+    switch (j)
+    {
+    /* Add book */
+    case 1:
+     count = add_book(i,l,count);
+
+      break;
+    case 2:
+      
+       display(l,count);
+
+      break;
+
+    case 3:
+      search_author(l,count);
+
+      break;
+
+    case 4:
+       search_book(l,count);
+
+      break;
+
+    case 5:
+       book_count(l,count);
+       
+      break;
+    case 6:
+      exit(0);
+    default:
+      printf("\nBlunder!!!Try\n");
+
+    }
+  }
+  return 0;
+}
---
+++ 3_Implementation/src/vf.c
@@ -0,0 +1,80 @@
+#include"inc.h"
+
+int add_book(int i, struct library *l, int count)
+{
+      printf("Enter book name = ");
+      scanf("%s", &l[i].bookname);
+
+      printf("Enter author name = ");
+      scanf("%s", &l[i].authorname);
+
+      printf("Enter pages = ");
+      scanf("%d", &l[i].pages);
+
+      printf("Enter price = ");
+      scanf("%f", &l[i].price);
+      count++;
+      return count;
+}
+
+void book_count(struct library *l, int count)
+{
+    printf("\n No of books in library : %d\n", count);
+    
+}
+
+void display(struct library *l, int count)
+{
+    printf("Information about book\n");
+      for (int i = 0; i < count; i++)
+      {
+        printf("Book name = %s", l[i].bookname);
+
+        printf("\t Author name = %s", l[i].authorname);
+
+        printf("\t  Pages = %d", l[i].pages);
+
+        printf("\t  Price = %f", l[i].price);
+
+        printf("\n");
+      }
+      
+}
+
+int menu(void)
+{
+    printf("\n\n1. Add books\n2. Display books\n");
+    printf("3. Search book by author\n");
+    printf("4. Search book by book name\n");
+    printf("5. Total count\n");
+    printf("6. Exit");
+    return 0;
+}
+
+void search_author(struct library *l, int count)
+{
+    char arNm[30];
+          printf("Author name : ");
+      scanf("%s", arNm);
+      for (int i = 0; i < count; i++)
+      {
+        if (strcmp(arNm, l[i].authorname) == 0)
+          printf("%s \t %s\t  %d\t  %f \t \n", l[i].bookname, l[i].authorname, l[i].pages, l[i].price);
+      }
+      
+}
+
+
+void search_book(struct library *l, int count)
+{
+      char  bookNm[30];
+      printf("Book name : ");
+      scanf("%s", bookNm);
+      for (int i = 0; i < count; i++)
+      {
+        if (strcmp(bookNm, l[i].bookname) == 0)
+          printf("%s \t %s \t %d \t %f \t \n", l[i].bookname, l[i].authorname, l[i].pages, l[i].price);
+      }
+
+
+}
